d9fa4ebd4369c57c575ad5914d5b75b7dec02d26
w-alanna <hartalanna2@gmail.com>
2024-02-21 09:46:17 -0500

    Completed Step 1
    I used a loop to assign values to nullRows because I kept getting errors when I tried to parallelize it.

--- qaqc.R
+++ qaqc.R
@@ -1,2 +1,35 @@
 # qaqc.R
 
+# ssh  hart187@compass.pnl.gov <- connect to COMPASS on powershell
+# /compass/datasets/fme_data_release/sensor_data/Level1/v0-9/ <- all the COMMPASS files
+# scp hart187@compass.pnl.gov:/compass/datasets/fme_data_release/sensor_data/Level1/v0-9/PTR_2022/PTR_20220701-20220731_L1_v0-9.csv .
+
+process <- function() {
+  library(dplyr)
+  #fme20002
+  #read in file
+  compassData <- read.csv("data\\COMPASS-dataset.csv")
+  
+  #filter na colmuns
+  summaryData <- compassData %>% filter(!complete.cases(value) | !complete.cases(F_OOB)) %>% group_by(research_name)
+  
+  #spliting dataframe based on research_name
+  dataSplit <- split(summaryData, f = summaryData$research_name)
+  
+  #getting list for standard deviation
+  deviationList <- split(compassData, f=compassData$research_name)
+  
+  #getting count for dataSplit
+  nullRows <- data.frame(matrix(ncol = 3, nrow = length(dataSplit)))
+  colnames(nullRows) <- c('Names', 'numOfNA', 'StandardDeviation')
+  
+  for(i in 1:length(dataSplit)) {
+    nullRows$Names[i] <- dataSplit[[i]]$research_name
+    nullRows$numOfNA[i] <- length(dataSplit[[i]]$value)
+    nullRows$StandardDeviation[i] <- sd(deviationList[[i]]$value)
+  }
+  
+  
+  
+}
+
