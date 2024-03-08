1d05e133a643d24437b810fe217e43b364ee218e
w-alanna <hartalanna2@gmail.com>
2024-03-08 10:24:58 -0500

    deleted comments to clean up script

---
+++ qaqc_main.R
@@ -0,0 +1,18 @@
+
+library(dplyr)
+compute_na_sd_forList <- function(f_site, f_year) {
+  #gets name of folder
+  file = paste(f_site, f_year, sep = "_")
+  folder = paste("data\\", file, "\\", sep = "")
+  
+  #gets the files from the folder
+  listOfFiles <- list.files(folder, pattern = "csv$", full.names = TRUE)
+  
+  list <- lapply(listOfFiles, compute_na_sd, mainsite=f_site)
+  
+  bind_rows(list) %>% return()
+}
+
+#average
+#out2 %>% ggplot(aes(date, average)) + geom_line() + facet_wrap(~research_name, scales="free")
+#out2 %>% ggplot(aes(date, p_sd)) + geom_line() + facet_wrap(~research_name, scales="free")
