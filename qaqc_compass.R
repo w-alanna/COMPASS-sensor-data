00e2da82a9540cac451c573ebc91adff71ee87ac
w-alanna <hartalanna2@gmail.com>
2024-05-31 08:59:57 -0400

    Added comments and indenting

---
+++ qaqc_compass.R
@@ -0,0 +1,69 @@
+library(readr)
+library(dplyr)
+library(lubridate)
+
+# Function to do the analysis for a file
+compute_na_sd <- function(filename) {
+	message("\treading ", basename(filename))
+	fileData <- read_csv(filename, show_col_types = FALSE)
+	#adds column holding the date
+	fileData <- fileData %>% mutate(date = as.Date(TIMESTAMP, format="%m/%d/%y"))
+	#gets the name of the site the data is from
+	sitename <- substring(basename(filename), 1,3)
+	
+	#summarizes the data
+  fileData %>% 
+    group_by(research_name, date) %>%
+    summarise(n_NA = sum(is.na(value)),
+    average = mean(value, na.rm = TRUE),
+    stdev = sd(value, na.rm = TRUE),
+  	mad=mad(value, na.rm=TRUE),
+  	site=sitename,
+    pct_NA = n_NA/(sum(!is.na(ID))), .groups="drop") %>% 
+  return()
+}
+
+
+#analysis of a folder using a slurm_id
+send_dir_path <- function(slurm_id, index, out_dir) {
+	
+  #gets list of the sub directories
+  all_data_dirs <- list.dirs("/compass/datasets/fme_data_release/sensor_data/Level1/v0-9", full.names=TRUE)[-1]
+  #chooses sub directory to be processed based on slurm_id
+  my_dir <- all_data_dirs[slurm_id]
+	message("processing ", my_dir)
+	
+	#gets the files in the folder choosen
+	files_in_dir <- list.files(my_dir, pattern="csv$", full.names=TRUE)
+	#sends the files to the compute_na_sd function to be processed
+	list_files <- lapply(files_in_dir, compute_na_sd)
+	finish_files <- bind_rows(list_files)	
+  # write results to CSV
+  out_file <- file.path(out_dir, paste0("example_output_", slurm_id, "_", index, ".csv"))
+  write.csv(finish_files, out_file, row.names = FALSE)
+	message("wrote out file ", out_file)
+}
+
+
+
+
+# ======= Start of main script; called from the run_example.sl script ======
+
+# Get arguments from the user / parent script
+args <- commandArgs(trailingOnly=TRUE)
+# The first argument is the SLURM task ID
+slurm_id <- as.integer(args[1])
+message("running with slurm id ", slurm_id)
+
+# batch size for the number of runs to process on a single node
+# We want to take advantage of the multi-core nature of the compute nodes
+batch_size <- 10
+
+# directory to write the output CSV files to
+out_dir <- "/compass/fme200002/ahart/COMPASS-sensor-data/output_dir"
+
+# Run the thing and produce output file for each iteration in parallel
+for (i in batch_size) {
+  send_dir_path(slurm_id, i, out_dir)
+}
+
